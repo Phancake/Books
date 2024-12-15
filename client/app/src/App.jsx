9fdfc64cb986e096be4e4153f8f4890062292add
Phancake <phancake2003@gmail.com>
2024-12-15 16:59:43 +0800

    added frontend

--- client/app/src/App.jsx
+++ client/app/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [releaseYear, setReleaseYear] = useState(0);
   useEffect(() => {
     fetchBooks();
-  });
+  }, []);
 
   const fetchBooks = async () => {
     try {
@@ -18,14 +18,13 @@ function App() {
       console.log(err)
     }
   }
-
   const addBook = async () => {
     const bookData = {
-      title,
+      title: title,
       release_year: releaseYear,
     };
     try {
-      const response = fetch("http://127.0.0.1:8000/api/books/add/", {
+      const response = await fetch("http://127.0.0.1:8000/api/books/add/", {
         method: "POST",
         headers: {
           'Content-Type': 'application/json',
@@ -44,15 +43,17 @@ function App() {
       <h1> Book Website</h1>
 
       <div>
-        <input type="text" placeholder="Book Title..." onChange={(e) => setTitle.apply(e.target.value)}/>
-        <input type="number" placeholder="Release Year..." onChange={(e) => setReleaseYear.apply(e.target.value)}/>
+        <input type="text" placeholder="Book Title..." onChange={(e) => setTitle(e.target.value)}/>
+        <input type="number" placeholder="Release Year..." onChange={(e) => setReleaseYear(parseInt(e.target.value))}/>
         <button onClick={addBook}>Add Book</button>
       </div>
       {books.map((book) => {
-      <div>
+        return (
+      <div key={book.id}>
         <p>Title: {book.title}</p>
         <p>Release Year: {book.release_year}</p>
       </div>
+        );
       })}
     </>
   )
